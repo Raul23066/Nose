a786a5fd5796b22a73263e1e8dddd8233e4bb4fc
Raul23066 <140854151+Raul23066@users.noreply.github.com>
2023-12-24 02:41:18 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,12 +5,12 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `𝐋𝐚𝐫𝐢𝐨𝐬 𝐕𝟐.𝟎 ⚙️ ${pesan}`;
+  const oi = `𝓡𝓔𝓧 𝓑𝓞𝓣 ⚙️ ${pesan}`;
   let teks = `𝐀𝐜𝐭𝐢𝐯𝐞𝐧𝐬𝐞 𝐇𝐩𝐭𝐚𝐬!!\n\n ${oi}\n\n 𝐌𝐞𝐧𝐜𝐢𝐨𝐧𝐞𝐬:\n\n`;
   for (const mem of participants) {
     teks += `⚙️ @${mem.id.split('@')[0]}\n`;
   }
-  teks += ` 𝐋𝐚𝐫𝐢𝐨𝐬 𝐕𝟐.𝟎 ⚙️`;
+  teks += ` 𝓡𝓔𝓧 𝓑𝓞𝓣 ⚙️`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
