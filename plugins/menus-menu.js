700c434c5513bf169f8f5867fc3a349ff754e339
Raul23066 <140854151+Raul23066@users.noreply.github.com>
2024-06-10 20:17:03 -0500

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -22,12 +22,13 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
     const str = ` 
-     NAJERA BOT ⚙️
+     BOT MONSTER ⚙️
 
    𝐇𝐨𝐥𝐚, ${taguser}
 
-𝐂𝐑𝐄𝐀𝐃𝐎𝐑 𝐃𝐄𝐋 𝐁𝐎𝐓: NAJERA BOT ⚙️
-𝐍𝐔𝐌𝐄𝐑𝐎: wa.me/50258397168
+𝐂𝐑𝐄𝐀𝐃𝐎𝐑 𝐃𝐄𝐋 𝐁𝐎𝐓: BOT MONSTER ⚙️
+𝐍𝐔𝐌𝐄𝐑𝐎: wa.me/525587037692
+wa.me/528683088045
 𝐅𝐄𝐂𝐇𝐀: ${date}
 
 𝐔𝐬𝐚 𝐞𝐥 𝐒𝐢𝐠𝐮𝐢𝐞𝐧𝐭𝐞 𝐂𝐨𝐦𝐚𝐧𝐝𝐨 𝐩𝐚𝐫𝐚 𝐯𝐞𝐫 𝐏𝐫𝐞𝐜𝐢𝐨𝐬 𝐝𝐞𝐥 𝐁𝐨𝐭
@@ -393,7 +394,7 @@ ${usedPrefix}menu2
 ⚙️ ${usedPrefix}saveimage
 ⚙️ ${usedPrefix}viewimage
 
-    NAJERA BOT⚙️ `.trim();
+    BOT MONSTER ⚙️ `.trim();
     if (m.isGroup) {
       // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
